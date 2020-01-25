5339db5d64e3938045f0b148f8c9cb25a6e27beb
nanhuazhang <nanhua.zhang@shopee.com>
2020-01-25 15:17:48 +0800

    fix port 8080 ->80

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 git pull
 sudo docker image build -t airpay_country_admin:1.0 .
 sudo docker rm -f apc_container
-sudo docker run -p 8080:80 -d --name apc_container airpay_country_admin:1.0
+sudo docker run -p 80:80 -d --name apc_container airpay_country_admin:1.0
